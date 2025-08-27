2345f4c7bab8da451849f2ae0743e958cdbed6ba
aman <kumaramansingh137@gmail.com>
2025-08-27 13:32:25 +0530

    resume updated

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import AmanKumar from "../../assets/AmanKumar.pdf";
+import AmanKumar from "../../assets/Aman-Kumar.pdf";
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={AmanKumar} download className="btn">
+      <a href={AmanKumar} download="Aman-Kumar.pdf" className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
